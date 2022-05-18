441234c9b2d62ec2d801eea602932983658f965d
Vadim Makarov <gitlabs541@gmail.com>
2022-05-18 09:59:38 +0300

    Создана заготовка проекта

---
+++ main-project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Laboratory work #1. GIT\n";
+	cout << "Variant #1. Marathon of result\n";
+	cout << "Author: Vadim Makarov\n";
+	return 0;
+}
