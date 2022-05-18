53645504b1e244a97f5e445c305a32d7566882dd
SexGodOdin <85231809+SexGodOdin@users.noreply.github.com>
2022-05-18 10:01:36 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	cout << "Laboratory work #1. GIT\n";
-	cout << "Variant #1. Marathon of result\n";
+	cout << "Variant #1. Marathon of results\n";
 	cout << "Author: Vadim Makarov\n";
 	return 0;
 }
